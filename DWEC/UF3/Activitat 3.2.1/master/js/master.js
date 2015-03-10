eb744825b178361762d2605a0c89da952acd29f5
cep <cep@A3--PC17.cepnet1.net>
2015-03-10 12:25:58 +0100

    Modificando ficheros

--- DWEC/UF3/Activitat 3.2.1/master/js/master.js
+++ DWEC/UF3/Activitat 3.2.1/master/js/master.js
@@ -1,13 +1,15 @@
-var master = ( function () 
+var master = (function()
 {
     var filaAleatoria = [];
-    
+    var filaUsuario = [];
+
+    //Función para generar una fila aleatoria
     function generarFilaAleatoria()
     {
-        for(var i = 0; i < 5; i++)
+        for (var i = 0; i < 5; i++)
         {
             var clase = "";
-            switch(Math.floor(Math.random()*6))
+            switch (Math.floor(Math.random() * 6))
             {
                 case 0:
                     clase = "I";
@@ -25,52 +27,78 @@ var master = ( function ()
                     clase = "V";
                 case 5:
                     clase = "VI";
-                    break; 
+                    break;
             }
             filaAleatoria[i] = clase;
         }
         console.log("Fila aleatoria: " + filaAleatoria);
     }
-    
+
     function comprobarBolas(bolas)
     {
         var existen = [];
         var existenPlus = [];
         var nada = [];
         var final = [];
-        
-        for(var i = 0; i < 5; i++)
+
+        for (var i = 0; i < 5; i++)
         {
             res = comprobarBola(bolas[i]);
-            
+
             if (res == "no")
             {
                 nada.push(res);
             }
-            else if(res == "ko")
+            else if (res == "ko")
             {
                 existen.push(res);
             }
-            else if(res == "ok")
+            else if (res == "ok")
             {
                 existenPlus.push(res);
             }
         }
-        
-        if(existenPlus.length > 0)
+
+        if (existenPlus.length > 0)
         {
             final.push.apply(final, existenPlus);
         }
-        if(existen.length > 0)
+        if (existen.length > 0)
         {
             final.push.apply(final, existen);
         }
-        if(nada.length > 0)
+        if (nada.length > 0)
         {
             final.push.apply(final, nada);
         }
-        
+
         return final;
+    }
+    
+    function comprobarBolas()
+    {
+        var resultado = [];
+        
+        for(var i = 0; i < 5; i++)
+        {
+            var res = "no";
+            
+            for(var j = 0; j < 5; i++)
+            {
+                if(filaUsuario[i] == filaAleatoria[j])
+                {
+                    res = "ko";
+                    
+                    if(i == j)
+                    {
+                       res = "ok"; 
+                    }
+                }
+                resultado.push(res);
+            }
+        }
+        
+        return resultado;
     }
     
     function comprobarBola(bola)
@@ -80,14 +108,14 @@ var master = ( function ()
         var res = "no";
         var i = 0;
         var claseBola = document.getElementById(bola).getAttribute("class").valueOf().split(" ")[1];
-        
+
         while (i < 5 && res == "no")
         {
-            if(filaAleatoria[i] == claseBola)
+            if (filaAleatoria[i] == claseBola)
             {
                 res = "ko";
-            
-                if(num == i)
+
+                if (num == i)
                 {
                     res = "ok";
                 }
@@ -97,10 +125,15 @@ var master = ( function ()
         console.log("Resultado comprobación: Bola " + num + " + clase " + claseBola + " = " + res);
         return res;
     }
-    
-    return{                 
-        pub_comprobarBolas : comprobarBolas,
-        //pub_comprobarBola : comprobarBola,
-        pub_generarFilaAleatoria : generarFilaAleatoria
-    } 
+
+    function setFilaUsuario(fila)
+    {
+        filaUsuario = fila;
+    }
+
+    return{
+        pub_comprobarBolas: comprobarBolas,
+        pub_generarFilaAleatoria: generarFilaAleatoria,
+        pub_setFilaUsuario: setFilaUsuario
+    }
 }());
--- DWEC/UF3/Activitat 3.2.1/master/js/masterui.js
+++ DWEC/UF3/Activitat 3.2.1/master/js/masterui.js
@@ -1,8 +1,8 @@
-var masterui = ( function () {
+var masterui = (function() {
     var acabado = false;
     var inicio = true;
-    var jugada = [];
-    
+    var resultados = [];
+
     //Función de control de dificultad en slider
     function slider() {
         config.pub_setDificultad(10);
@@ -11,28 +11,28 @@ var masterui = ( function () {
             min: 1,
             max: 10,
             step: 1,
-            slide: function ( event, ui ) {
+            slide: function(event, ui) {
                 $("#dificultad").val(ui.value);
                 config.pub_setDificultad(ui.value);
             }
         });
         $("#dificultad").val($("#slider").slider("value"));
-    }               
-    
+    }
+
     //Función para cambio de color de bola
-    function cambioColorBola(){
-        
-        $(".selector").click(function(){
+    function cambioColorBola() {
+
+        $(".selector").click(function() {
             var parent = $(this).attr("parent");
             var val = $(this).attr("class").split(" ")[1];
             $("#" + parent).removeClass("no I II III IV V VI");
-            $("#" + parent).addClass(val);            
+            $("#" + parent).addClass(val);
         });
     }
-    
+
     //Función para iniciar/continuar con el juego
     function jugarPartida() {
-        $("#btn-juego").click(function () {
+        $("#btn-juego").click(function() {
             if (!acabado)
             {
                 if (inicio)
@@ -40,31 +40,32 @@ var masterui = ( function () {
                     master.pub_generarFilaAleatoria();
                     inicio = false;
                 }
+                getFilaUsuariohtml();
+                resultados = master.pub_comprobarBolas();
                 generarFila();
             }
             else
             {
                 limpiarJugadas();
-                limpiarBolas();
-                
+
                 acabado = true;
                 inicio = true;
             }
         });
     }
-    
+
     //Función para obtener el color de una bola recibida por parámetro
     function getClassBola(bola)
     {
         return document.getElementById(bola).getAttribute("class").valueOf();
     }
-    
+
     //Función para obtener el número de intento en el que se encuentra el usuario
     function getNumIntento()
     {
         return document.getElementById("intento").textContent;
     }
-    
+
     //Función que controla el número de intentos jugados y permite que se pueda
     //seguir jugando o no.
     function contador()
@@ -83,13 +84,13 @@ var masterui = ( function () {
         }
         document.getElementById("intento").textContent = numIntento;
     }
-    
+
     //Función que genera una fila de una partida jugada por el usuario
     function generarFila()
     {
         var fila = "";
         var resultados = [];
-        
+
         fila += "<div class='clearfix fila text-center'>\n\
                     <div class='col-lg-12 col-md-12 col-sm-12 contenido-fila'>\n\
                     <div class='col-lg-1 col-md-1 col-sm-1 col-xs-1 paddingcero'>\n\
@@ -121,8 +122,6 @@ var masterui = ( function () {
         fila += "<span class='num-intento'>" + getNumIntento() + "</span></div>";
         fila += "<div class='col-xs-1'>\n\
                     <div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>";
-        resultados = master.pub_comprobarBolas(getJugada());
-        
         fila += "<div class='" + resultados[0] + " bola'></div>";
         fila += "</div>\n\
                     </div>\n\
@@ -150,43 +149,44 @@ var masterui = ( function () {
                     </div>";
 
         contador();
-
         $("#jugadas").prepend(fila)
         limpiarBolas();
+    }
+
+    function getFilaUsuariohtml()
+    {
+        var jugada = [];
+
+        for (var i = 0; i < 6; i++)
+        {
+            jugada[i] = "bola" + i;
+        }
+
+        master.pub_setFilaUsuario(jugada);
     }
     
     //Función para limpiar las jugadas anteriores
     function limpiarJugadas()
     {
         $("#jugadas").empty();
+        limpiarBolas();
     }
-    
+
     //Función para limpiar las bolas
     function limpiarBolas()
     {
-        for(var i = 0; i < 5; i++)
+        for (var i = 0; i < 5; i++)
         {
             $("#bola" + i).removeClass("I II III IV V VI");
-            $("#bola" + i).addClass("no");   
+            $("#bola" + i).addClass("no");
         }
     }
-    
-    function getJugada()
-    {
-        var jugada = [];
-        
-        for(var i = 0; i < 6; i++)
-        {
-            jugada[i] = "bola"+i;
-        }
-        
-        return jugada;
-    }
-    
+
     //Devolución de métodos públicos
-    return{                 
-        pub_slider : slider,
-        pub_cambioColorBola : cambioColorBola,
-        pub_jugarPartida : jugarPartida
-    } 
+    return{
+        pub_slider: slider,
+        pub_cambioColorBola: cambioColorBola,
+        pub_jugarPartida: jugarPartida
+        
+    }
 }());
